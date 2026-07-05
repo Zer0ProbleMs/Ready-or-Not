7e7bce00603fbe7230d034e858a3c23f560b4d78
Zer0ProbleMs <daris.kkc2000@gmail.com>
2026-07-05 17:01:21 +0200

    New Animations
    Idle, Walking and Running animations have been added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,6 +15,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField] Image staminaBar;
     [SerializeField] Image staminaBackground;
     [SerializeField] GameObject stamina;
+
+    Animator playerAnimator;
+    SpriteRenderer spriteRenderer;
     
     [FormerlySerializedAs("MoveSpeed")]
     [Header("Player Status:")]
@@ -22,8 +25,9 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float defaultSpeed = 5f;
     [SerializeField] private float sprintSpeed = 7f;
     [SerializeField] private float currentStamina = 100f;
-    [SerializeField] private bool isRunning = false;
+    [SerializeField] private bool runPressed = false;
     [SerializeField] private bool canRun = true;
+    [SerializeField] private bool isRunning = false;
     [SerializeField] private bool playerControl = true;
     
     private float maxStamina = 100;
@@ -31,8 +35,15 @@ public class PlayerController : MonoBehaviour
     private float staminaUp = 5f;
     private Vector2 _movementinput;
     private float currentOpacity;
+    private bool _context;
     
     #endregion
+
+    private void Awake()
+    {
+        playerAnimator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     private void Start()
     {
@@ -49,47 +60,56 @@ public class PlayerController : MonoBehaviour
     {
         Movement();
         StaminaBar();
+        Animation();
     }
 
     #region General Movement
     
     public void MoveInput(InputAction.CallbackContext context) // Check for an input, ZQSD, and gives the right value depending on which it is
     {
+        
+        _context = context.canceled;
         _movementinput = context.ReadValue<Vector2>(); // Uses Vector2 to give values, Z is 1 on the y-axis, D is 1 on the x-axis, and the opposite for the rest
-        _movementinput = _movementinput.normalized;
     }
     
     public void SprintInput(InputAction.CallbackContext context)
     {
-        isRunning = context.ReadValueAsButton(); // Sends true if left shift is pressed, otherwise false
+        runPressed = context.ReadValueAsButton(); // Sends true if left shift is pressed, otherwise false
     }
 
     public void Movement()
     {
-        if (isRunning && currentStamina > 0 && canRun)
+        if (isRunning)
             moveSpeed = sprintSpeed; // If the player is holding shift, then the movement is faster
         
         else
             moveSpeed = defaultSpeed; // Otherwise the player moves at default speed
-
+        
         if (!playerControl) // Checks if the player is allowed to move or not
             rb.linearVelocity = new Vector2(0, 0); // Stops the player from moving
         else
-            rb.linearVelocity = _movementinput * moveSpeed; // The player's velocity if the player is moving
+            rb.linearVelocity = _movementinput.normalized * moveSpeed; // The player's velocity if the player is moving
+        
+        if (_movementinput.x > 0)
+            spriteRenderer.flipX = true;
+        else if (_movementinput.x < 0)
+            spriteRenderer.flipX = false;
     }
 
     public void StaminaBar()
     {
         staminaBar.color = new Color(staminaBar.color.r, staminaBar.color.g, staminaBar.color.b, currentOpacity);
         staminaBackground.color = new Color(staminaBackground.color.r, staminaBackground.color.g, staminaBackground.color.b, currentOpacity);
+        isRunning = false;
         
-        if (isRunning && rb.linearVelocity.magnitude > 0 && canRun) // Checks if the player is moving, wants to run and has stamina
+        if (runPressed && rb.linearVelocity.magnitude > 0 && canRun) // Checks if the player is moving, wants to run and has stamina
         {
             StopAllCoroutines(); // Stops the stamina from recharging
             currentOpacity = 1;
             stamina.SetActive(true);
             currentStamina -= staminaDown * Time.fixedDeltaTime; // Lowers stamina by a fixed rate
             staminaBar.fillAmount = currentStamina / maxStamina;
+            isRunning = true;
         }
         else
             StartCoroutine(RechargeStaminaBar()); // If the player isn't running, then we start the recharge stamina coroutine
@@ -128,4 +148,21 @@ public class PlayerController : MonoBehaviour
     }
     
     #endregion
+
+    public void Animation()
+    {
+        if (_movementinput.magnitude > 0)
+        {
+            
+            playerAnimator.SetBool("isWalking", true);
+            playerAnimator.SetBool("isRunning", isRunning);
+            playerAnimator.SetFloat("InputX", _movementinput.x);
+            playerAnimator.SetFloat("InputY", _movementinput.y);
+            playerAnimator.SetFloat("LastInputX", _movementinput.x);
+            playerAnimator.SetFloat("LastInputY", _movementinput.y);
+        }
+        else playerAnimator.SetBool("isWalking", false);
+        
+
+    }
 }
