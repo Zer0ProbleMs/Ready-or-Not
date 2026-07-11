f32ac4528c596470f39eef6c967e3f0499b8cc91
Zer0ProbleMs <daris.kkc2000@gmail.com>
2026-07-11 17:19:38 +0200

    Save before multiplayer testing
    This save solves the animation and stamina issues of the animatronic, and is necessary in case of mistakes with my next tests

--- Assets/Scripts/StaminaCircleManager.cs
+++ Assets/Scripts/StaminaCircleManager.cs
@@ -43,26 +43,45 @@ public class StaminaCircleManager : MonoBehaviour
         }
         else
         {
-            
+            StartCoroutine(StaminaUp());
         }
         
         StaminaCircleOpacity(currentOpacity);
     }
 
-    public void StaminaDown()
+    private void StaminaDown()
     {
         currentOpacity = 1;
         stamina.SetActive(true);
         staminaCircle.fillAmount -= 0.67f * Time.fixedDeltaTime;
+        if (staminaCircle.fillAmount <= 0) staminaCircle.fillAmount = 0;
     }
 
-    public void StaminaCircleOpacity(float opacity) // Function to make accessing my stamina bar's opacity easier
+    private IEnumerator StaminaUp()
+    {
+        yield return new WaitForSeconds(1f);
+        staminaCircle.fillAmount += 0.67f * Time.fixedDeltaTime;
+        if (staminaCircle.fillAmount >= 1)
+        {
+            staminaCircle.fillAmount = 1;
+            yield return new WaitForSeconds(2f);
+            while (currentOpacity >= 0) // As long as the currentOpacity is higher than 0
+            {
+                currentOpacity -= 5f * Time.fixedDeltaTime; //Lower it (i wanna try doing it in another thread)
+                yield return new WaitForSeconds(0.05f); // Every 0.05 seconds (because a while loop alone is too fast)
+            }
+            stamina.SetActive(false);// Once done, turn off the GameObject
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+
+    private void StaminaCircleOpacity(float opacity) // Function to make accessing my stamina bar's opacity easier
     {
         staminaCircle.color = new Color(staminaCircle.color.r, staminaCircle.color.g, staminaCircle.color.b, opacity);
         staminaBackground.color = new Color(staminaBackground.color.r, staminaBackground.color.g, staminaBackground.color.b, opacity);
     }
 
-    public void StaminaCircleMaterialOpacity(float opacity)
+    private void StaminaCircleMaterialOpacity(float opacity)
     {
         staminaCircle.material.color = new Color(1, 1, 1, opacity);
         staminaBackground.material.color = new Color(1, 1, 1, opacity);
